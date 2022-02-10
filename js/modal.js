9c6c568ea4c4cd677379e422adf71c04934308f2
Vale Ferro <dg.vale.ferro@gmail.com>
2022-02-10 20:57:11 -0300

    gallery > lightbox

--- js/modal.js
+++ js/modal.js
@@ -1,12 +1,3 @@
-// document.addEventListener("click", function(e) {
-//     if (e.target.classList.contains("gallery-item")) {
-//         const src = e.target.getAttribute("src").replace("-th", "").replace("img-", "photo-");
-//         document.querySelector(".modal-img").src = src;
-//         const myModal = new bootstrap.Modal(document.getElementById('gallery-modal'));
-//         myModal.show();
-//     }
-// })
-
 $('.modalGallery__img').click(function(e) {
     var img = e.target.src;
     var modal = '<div class="modal" id="modal"><img src="' + img + '" class="modal__img"><div class="modal__shut" id="modal__shut">X</div></div>'
